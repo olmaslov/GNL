b5467ee87baf2e84bf0186812d455e5d03ff23bb
omaslov <omaslov@student.unit.ua>
2018-01-04 18:22:38 +0200

    4 error basic, not work multi fd, 6 error moulitest

--- gnl_in/get_next_line.c
+++ gnl_in/get_next_line.c
@@ -12,13 +12,13 @@
 
 #include "get_next_line.h"
 
-t_file	*listfilenew(void const *content, size_t content_size, int fd, t_file *list)
+t_file	*listfilenew(void const *content, size_t cs, int fd, t_file *list)
 {
 	t_file *new;
 
 	if (!(new = (t_file *)malloc(sizeof(*new))))
 		return (NULL);
-	if (!(new->content = malloc(content_size)))
+	if (!(new->content = malloc(cs)))
 	{
 		free(new);
 		return (NULL);
@@ -31,9 +31,10 @@ t_file	*listfilenew(void const *content, size_t content_size, int fd, t_file *li
 		new->next = list;
 		return (new);
 	}
+	ft_bzero(new->content, cs);
 	new->fd = fd;
-	new->content_size = content_size;
-	ft_memcpy(new->content, content, content_size);
+	new->content_size = cs;
+	ft_strcpy(new->content, content);
 	new->next = list;
 	return (new);
 }
@@ -49,40 +50,42 @@ t_file	*searchfd(int fd, t_file *list)
 	{
 		listf = listf->next;
 		if (listf == temp)
-			break;
+			break ;
 	}
 	return (listf);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	char	buf1[BUFF_SIZE + 1];
-	char	*buff;
-	char	*s2;
-	int		ret;
-	int		j;
-	static 	t_file	*list;
-	int		s;
-	size_t x;
+	char			buf1[BUFF_SIZE + 1];
+	char			*buff;
+	char			*s2;
+	int				ret;
+	int				j;
+	static t_file	*list;
+	int				s;
+	size_t			x;
 
 	ret = 0;
 	buff = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
+	ft_bzero(buf1, BUFF_SIZE + 1);
 	if (list != NULL && list->fd != fd)
 	{
 		list = searchfd(fd, list);
 	}
 	if (list != NULL && list->fd == fd)
 	{
-//		while (list->fd != fd && list->next != NULL)
-
 		s2 = list->content;
 		x = 0;
+		if (*s2 == '\0')
+			return 0;
 		while (*s2 && *s2 != '\n')
 			buff[x++] = *s2++;
-		if (*s2 == '\n')
+		if (*s2 == '\n' || *s2 == '\0')
 		{
-			list->content = s2 + 1;
+			list->content = ft_strdup(s2 + 1);
 			*line = buff;
+			free(buff);
 			return (1);
 		}
 		ret = x;
@@ -95,23 +98,22 @@ int		get_next_line(const int fd, char **line)
 		while (j < BUFF_SIZE)
 		{
 			buff[ret] = buf1[j];
-			if (buf1[j] == '\n' || buf1[j] == '\0')
+			if (buf1[j] == '\n')
 			{
-				if (buf1[j] == EOF)
-					return -1;
 				j++;
 				buf1[BUFF_SIZE] = '\0';
 				buff[ret] = '\0';
 				if (list == NULL)
 				{
-					list = listfilenew((void *)(buf1 + j), ft_strlen(buf1 + j), fd, NULL);
+					list = listfilenew((void *)(buf1 + j),
+					ft_strlen(buf1 + j), fd, NULL);
 					list->next = list;
 				}
 				else if (list->fd != fd)
 				{
-					list->next = listfilenew((void *)(buf1 + j), ft_strlen(buf1 + j), fd, list);
+					list->next = listfilenew((void *)(buf1 + j),
+					ft_strlen(buf1 + j), fd, list);
 					list = list->next;
-
 				}
 				else
 				{
@@ -119,14 +121,26 @@ int		get_next_line(const int fd, char **line)
 					list->fd = fd;
 				}
 				*line = buff;
+				free(buff);
 				return (1);
+			}
+			if (buf1[j] == '\0' && buf1[j - 1] == '\n')
+			{
+				*line = buff;
+				free(buff);
+				return (0);
 			}
 			buff = ft_strjoin(ft_strnew(BUFF_SIZE + 1), buff);
 			ret++;
 			j++;
 		}
+		ft_bzero(buf1, BUFF_SIZE + 1);
 	}
 	if (s == 0 && buff[0] != '\0')
+	{
 		*line = buff;
+		free(buff);
+		return (1);
+	}
 	return (0);
 }
